31dd7478e8bca3790bb324d83e8e95c92d955a29
Yashwanth <Yashwanth@LAPTOP-8UCC259I>
2023-07-30 16:31:14 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryListComponent } from './feauters/category/category-list/category-list.component';
+import { AddCategoryComponent } from './feauters/category/add-category/add-category.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin/categories',
+    component: CategoryListComponent
+  },
+  {
+    path: 'admin/categories/add',
+    component: AddCategoryComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
